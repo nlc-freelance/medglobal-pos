a0b7c1e6944d6e4cdb755d0af0fc567ba05b0458
ncanada <canadanataliaa@gmail.com>
2024-07-19 17:31:15 +0800

    Adjust json key request for creation of stock transfer

--- lib/features/stock_management/stock_transfer/domain/entities/new_stock_transfer.dart
+++ lib/features/stock_management/stock_transfer/domain/entities/new_stock_transfer.dart
@@ -4,26 +4,26 @@ part 'new_stock_transfer.g.dart';
 
 @JsonSerializable()
 class NewStockTransfer {
-  final int? sourceBranchId;
-  final int? destinationBranchId;
+  final int? fromBranch;
+  final int? toBranch;
   final bool? isAutoFill;
 
   NewStockTransfer({
-    this.sourceBranchId,
-    this.destinationBranchId,
+    this.fromBranch,
+    this.toBranch,
     this.isAutoFill = false,
   });
 
   Map<String, dynamic> toJson() => _$NewStockTransferToJson(this);
 
   NewStockTransfer copyWith({
-    int? sourceBranchId,
-    int? destinationBranchId,
+    int? fromBranch,
+    int? toBranch,
     bool? isAutoFill,
   }) {
     return NewStockTransfer(
-      sourceBranchId: sourceBranchId ?? this.sourceBranchId,
-      destinationBranchId: destinationBranchId ?? this.destinationBranchId,
+      fromBranch: fromBranch ?? this.fromBranch,
+      toBranch: toBranch ?? this.toBranch,
       isAutoFill: isAutoFill ?? this.isAutoFill,
     );
   }
