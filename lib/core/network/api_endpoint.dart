db626bc6683eebf768beb77fda16d5cdb3ec0f60
ncanada <canadanataliaa@gmail.com>
2024-08-13 16:31:27 +0800

    Change baseUrl for preprod

--- lib/core/network/api_endpoint.dart
+++ lib/core/network/api_endpoint.dart
@@ -1,13 +1,12 @@
-import 'package:flutter/foundation.dart';
-
 class ApiEndpoint {
   final String path;
 
   ApiEndpoint(this.path);
 
   static String baseUrl(path) {
-    String sandboxBaseUrl = 'https://rgg6sphmg6.execute-api.ap-southeast-1.amazonaws.com/sandbox/';
-    String devBaseUrl = 'https://059iccwuk4.execute-api.ap-southeast-1.amazonaws.com/dev';
+    // String sandboxBaseUrl = 'https://rgg6sphmg6.execute-api.ap-southeast-1.amazonaws.com/sandbox/';
+    // String devBaseUrl = 'https://059iccwuk4.execute-api.ap-southeast-1.amazonaws.com/dev';
+    String preProdBaseUrl = 'https://w9u9b44xu9.execute-api.ap-southeast-1.amazonaws.com/preprod';
 
     // if (path.contains('/suppliers')) {
     //   url = kDebugMode ? sandboxBaseUrl : 'https://7ayvpkcn48.execute-api.ap-southeast-1.amazonaws.com/dev';
@@ -29,7 +28,8 @@ class ApiEndpoint {
     //   url = sandboxBaseUrl;
     // }
 
-    return kDebugMode ? sandboxBaseUrl : devBaseUrl;
+    return preProdBaseUrl;
+    // return kDebugMode ? sandboxBaseUrl : devBaseUrl;
   }
 
   /// Supplier
