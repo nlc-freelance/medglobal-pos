713e6fb84ada915ec9340e06a592b86719a04f66
ncanada <canadanataliaa@gmail.com>
2024-08-22 13:33:55 +0800

    [temp-fix] temporary fix for pos register selection

--- lib/pos/register/data/api/register_api.dart
+++ lib/pos/register/data/api/register_api.dart
@@ -1,8 +1,10 @@
 import 'package:medglobal_admin_portal/core/network/api_service.dart';
 import 'package:medglobal_admin_portal/pos/register/data/dto/register_dto.dart';
+import 'package:medglobal_admin_portal/pos/register/domain/entities/register_shift/register.dart';
 
 abstract class RegisterApi {
   Future<List<RegisterDto>> getAllRegisters();
+  Future<List<Register>> getRegisters([int page]);
 }
 
 class RegisterApiImpl implements RegisterApi {
@@ -20,6 +22,21 @@ class RegisterApiImpl implements RegisterApi {
       );
 
       return response.items!;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<Register>> getRegisters([int page = 1]) async {
+    try {
+      final response = await _apiService.collection<RegisterDto>(
+        '/registers',
+        queryParams: {'page': page, 'size': 10},
+        converter: RegisterDto.fromJson,
+      );
+
+      return response.items?.map((item) => item.toEntity()).toList() ?? <Register>[];
     } catch (e) {
       rethrow;
     }
