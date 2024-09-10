94cdb4e8c85bda791bd8924e4bac6c918af5416d
ncanada <canadanataliaa@gmail.com>
2024-09-10 17:06:27 +0800

    [fix] change variant form to a dialog, uncomment validations for required variant fields

--- lib/portal/product_management/presentation/pages/product_details/widgets/inventory_and_variants/variants/variant_form.dart
+++ lib/portal/product_management/presentation/pages/product_details/widgets/inventory_and_variants/variants/variant_form.dart
@@ -1,11 +1,10 @@
-import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medglobal_admin_portal/core/core.dart';
 import 'package:medglobal_admin_portal/portal/product_management/domain/entities/product/variant.dart';
-import 'package:medglobal_admin_portal/portal/product_management/presentation/cubit/variant_form_ui/variant_form_ui_cubit.dart';
 import 'package:medglobal_admin_portal/portal/product_management/presentation/cubit/product_form/product_form_cubit.dart';
 import 'package:medglobal_admin_portal/portal/product_management/presentation/cubit/variant_form/variant_form_cubit.dart';
+import 'package:medglobal_admin_portal/portal/product_management/presentation/cubit/variant_form_ui/variant_form_ui_cubit.dart';
 import 'package:medglobal_admin_portal/portal/product_management/presentation/pages/product_details/widgets/inventory_and_variants/inventory_per_branch/inventory_per_branch.dart';
 import 'package:medglobal_admin_portal/portal/product_management/presentation/pages/product_details/widgets/inventory_and_variants/stock_keeping_unit/stock_keeping_unit_details.dart';
 import 'package:medglobal_shared/medglobal_shared.dart';
@@ -19,7 +18,6 @@ class VariantForm extends StatefulWidget {
     required this.warningStockController,
     required this.idealStockController,
     required this.costController,
-    required this.formKey,
   });
 
   final TextEditingController nameController;
@@ -27,7 +25,6 @@ class VariantForm extends StatefulWidget {
   final TextEditingController warningStockController;
   final TextEditingController idealStockController;
   final TextEditingController costController;
-  final GlobalKey<FormState> formKey;
 
   @override
   State<VariantForm> createState() => _VariantFormState();
@@ -35,6 +32,8 @@ class VariantForm extends StatefulWidget {
 
 class _VariantFormState extends State<VariantForm> {
   late VariantFormCubit _variantFormCubit;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? _hasMissingRequiredFieldsError;
 
   @override
   void initState() {
@@ -44,99 +43,102 @@ class _VariantFormState extends State<VariantForm> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocSelector<VariantFormCubit, VariantFormState, Variant?>(
-      selector: (state) => state.variant,
-      builder: (context, variant) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            PageSectionTitle.subsection(
-              title: 'Variations',
-              subtitle: Strings.subtitlePlaceholder,
-              action: UIButton.text(
-                'Close',
-                iconBuilder: (isHover) => Assets.icons.minusSquare.setColorOnHover(isHover),
-                onClick: () {
-                  context.read<VariantFormUiCubit>().hideVariantFormUi();
-
-                  /// If variant name is default, meaning the product has no variant initially
-                  /// Therefore if user decides to try and add a variant and change his mind,
-                  /// the values will still exist outside the variant form
-                  if (variant?.name != 'default') _variantFormCubit.resetForm();
-                },
+    return Form(
+      key: _formKey,
+      child: BlocSelector<VariantFormCubit, VariantFormState, Variant?>(
+        selector: (state) => state.variant,
+        builder: (context, variant) {
+          return Column(
+            children: [
+              const PageSectionTitle(
+                title: 'Variations',
+                subtitle: Strings.subtitlePlaceholder,
               ),
-            ),
-            DottedBorder(
-              color: UIColors.textRegular.withOpacity(0.3),
-              radius: const Radius.circular(12),
-              dashPattern: const [10],
-              borderType: BorderType.RRect,
-              borderPadding: const EdgeInsets.all(2),
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: UITextFormField.topLabel(
-                            label: 'Variant Name',
-                            hint: 'Enter variant name',
-                            controller: widget.nameController,
-                            showRequired: true,
-                            validator: (value) {
-                              if (value?.isEmpty == true || value == 'default') return 'Please enter a variant name.';
-                              return null;
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    const UIVerticalSpace(40),
-                    StockKeepingUnitDetails(
-                      skuController: widget.skuController,
-                      warningStockController: widget.warningStockController,
-                      idealStockController: widget.idealStockController,
-                      costController: widget.costController,
-                    ),
-                    const InventoryPerBranch(),
-                    const UIVerticalSpace(30.0),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: UIButton.secondary(
-                        'Save Variant',
-                        onClick: () {
-                          final variant = _variantFormCubit.state.variant;
-
-                          // if (widget.formKey.currentState?.validate() == true &&
-                          //     variant?.hasSuppliers == true &&
-                          //     variant?.hasBranchInventories == true) {
-                          if (variant?.id == null) {
-                            final newVariant = variant?.copyWith(id: -(const Uuid().v4().hashCode));
-                            context.read<ProductFormCubit>().addVariant(newVariant!);
-                          } else {
-                            context.read<ProductFormCubit>().updateVariant(variant!.id!, variant);
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const UIVerticalSpace(16),
+                      PageSectionTitle.subsection(title: 'Variant'),
+                      UITextFormField.topLabel(
+                        label: 'Variant Name',
+                        hint: 'Enter variant name',
+                        showRequired: true,
+                        controller: widget.nameController,
+                        validator: (value) {
+                          if (value?.isEmpty == true || value == 'default') {
+                            return 'Please enter a variant name.';
                           }
-
-                          context.read<VariantFormUiCubit>().hideVariantFormUi();
-                          _variantFormCubit.resetForm();
-                          // } else {
-                          //   ToastNotification.invalid(
-                          //       context, 'One of the required field is empty. Please check your inputs.');
-                          // }
+                          return null;
                         },
                       ),
-                    ),
-                  ],
+                      const UIVerticalSpace(40),
+                      StockKeepingUnitDetails(
+                        skuController: widget.skuController,
+                        warningStockController: widget.warningStockController,
+                        idealStockController: widget.idealStockController,
+                        costController: widget.costController,
+                      ),
+                      const InventoryPerBranch(),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const UIVerticalSpace(30.0),
-          ],
-        );
-      },
+              const UIVerticalSpace(30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  if (_hasMissingRequiredFieldsError != null) ...[
+                    UIText.labelSemiBold(_hasMissingRequiredFieldsError!, color: UIColors.buttonDanger),
+                    const Spacer(),
+                  ],
+                  UIButton.outlined(
+                    'Cancel',
+                    onClick: () {
+                      context.read<VariantFormUiCubit>().hideVariantFormUi();
+
+                      /// If variant name is default, meaning the product has no variant initially
+                      /// Therefore if user decides to try and add a variant and change his mind,
+                      /// the values will still exist outside the variant form
+                      if (variant?.name != 'default') _variantFormCubit.resetForm();
+
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const UIHorizontalSpace(8),
+                  UIButton.filled(
+                    'Save variant',
+                    onClick: () {
+                      final variant = _variantFormCubit.state.variant;
+
+                      if (_formKey.currentState?.validate() == true &&
+                          variant?.hasSuppliers == true &&
+                          variant?.hasBranchInventories == true) {
+                        if (variant?.id == null) {
+                          final newVariant = variant?.copyWith(id: -(const Uuid().v4().hashCode));
+                          context.read<ProductFormCubit>().addVariant(newVariant!);
+                        } else {
+                          context.read<ProductFormCubit>().updateVariant(variant!.id!, variant);
+                        }
+
+                        setState(() => _hasMissingRequiredFieldsError = null);
+
+                        _variantFormCubit.resetForm();
+                        context.read<VariantFormUiCubit>().hideVariantFormUi();
+                        Navigator.pop(context);
+                      } else {
+                        setState(() => _hasMissingRequiredFieldsError =
+                            'One of the required field is empty. Please check your inputs.');
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
